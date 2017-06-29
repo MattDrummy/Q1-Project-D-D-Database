e319852a4486190cc3ece2fb930435e2d11b80f6
Matthew Drumheller <MattDrummy@gmail.com>
2017-06-29 13:03:15 -0600

    updated Monster Page

--- assets/node/monsters.js
+++ assets/node/monsters.js
@@ -106,7 +106,9 @@ function createList(newData) {
   $searchResult.append('<ul></ul>')
   for (var i = 0; i < newData.length; i++) {
     var item = newData[i];
-    var $newListItem = $('<li><button class="listBtn btn btn-default" type="button">' + item.name + '</button></li>');
+    var $newListItem = $(`
+      <li><button class="listBtn btn btn-default" type="button">${item.name}</button></li>
+      `);
     var $listData = $('<article class="list"></article>');
     var $listSup = $('<article class="list"><button class="list listBtn btn btn-default">SPECIALS:</button></div>');
     var $listSupDiv = $('<div></div>');
@@ -115,26 +117,87 @@ function createList(newData) {
     $newListItem.append($listData);
 
     /*The following code represents the population of data, skip ahead to the next comment for the rest of the code*/
-    $listData.append('<p>Challenge Rating: ' + item.challenge_rating + '</p>');
-    $listData.append('<p>Type: ' + item.type + ' - Subtype: ' + (item.subtype === "" ? 'none' : item.subtype) + '</p>');
-    $listData.append('<p>Size: ' + item.size + '</p>');
-    $listData.append('<p>Alignment: ' + (item.alignment === "" ? 'none' : item.alignment) + '</p>');
-    $listData.append('<p>Armor Class: ' + item.armor_class + '</p>');
-    $listData.append('<p>Health - HitDice: ' + item.hit_dice + ' - HitPoints: ' + item.hit_points + '</p>');
-    $listData.append('<p>Condition Immunities: ' + (item.condition_immunities === "" ? 'none' : item.condition_immunities) + '</p>');
-    $listData.append('<p>Damage Immunities: ' + (item.damage_immunities === "" ? 'none' : item.damage_immunities) + '</p>');
-    $listData.append('<p>Damage Resistances: ' + (item.damage_resistances === "" ? 'none' : item.damage_resistances) + '</p>');
-    $listData.append('<p>Damage Vulnerabilities: ' + (item.damage_vulnerabilities === "" ? 'none' : item.damage_vulnerabilities) + '</p>');
-    $listData.append('<p>Senses: ' + item.senses + '</p>');
-    $listData.append('<p>Speed: ' + item.speed + '</p>');
-    $listData.append('<p>Strength: ' + item.strength + ' - Save: ' + item.strength_save + '</p>');
-    $listData.append('<p>Dexterity: ' + item.dexterity + ' - Save: ' + item.dexterity_save + '</p>');
-    $listData.append('<p>Constitution: ' + item.constitution + ' - Save: ' + item.constitution_save + '</p>');
-    $listData.append('<p>Intelligence: ' + item.intelligence + ' - Save: ' + item.intelligence_save + '</p>');
-    $listData.append('<p>Wisdom: ' + item.wisdom + ' - Save: ' + item.wisdom + '</p>');
-    $listData.append('<p>Charisma: ' + item.charisma + ' - Save: ' + item.charisma_save + '</p>');
-    $listData.append('<p>Perception: ' + item.perception + '</p>');
-    $listData.append('<p>Languages: ' + item.languages + '</p>');
+    $listData.append(`
+      <p><i>${item.size} ${item.type}</i></p>
+      `);
+    $listData.append(`
+      <p>Challenge Rating: ${item.challenge_rating}</p>
+      `);
+    $listData.append(`
+      <p>Alignment: ${item.alignment === "" ? 'none' : item.alignment}</p>
+      `);
+    $listData.append(`
+      <p>Armor Class: ${item.armor_class}</p>
+      `);
+    $listData.append(`
+      <p>Health - HitDice: ${item.hit_dice} - HitPoints: ${item.hit_points}</p>
+      `);
+    if (item.condition_immunities !== "") {
+      $listData.append(`
+        <p>Condition Immunities: ${item.condition_immunities}</p>
+        `);
+    }
+    if (item.damage_immunities !== "") {
+      $listData.append(`
+        <p>Damage Immunities: ${item.damage_immunities}</p>
+        `);
+    }
+    if (item.damage_resistances !== "") {
+      $listData.append(`
+        <p>Damage Resistances: ${item.damage_resistances}</p>
+        `);
+    }
+    if (item.damage_vulnerabilities !== "") {
+      $listData.append(`
+        <p>Damage Vulnerabilities: ${item.damage_vulnerabilities}</p>
+        `);
+    }
+    $listData.append(`
+      <p>Senses: ${item.senses}</p>
+      `);
+    $listData.append(`
+      <p>Speed: ${item.speed}</p>
+      `);
+    $listData.append(`
+      <p>Strength: ${item.strength}
+      (+${item.strength < 10 ? '-' + (5 - Math.floor(item.strength / 2)) : item.strength > 11 ? Math.floor((item.strength - 10) / 2) : 0})
+      ${ item.strength_save ? ' - Save: ' + item.strength_save : ''}</p>
+    `);
+    $listData.append(`
+      <p>Dexterity: ${item.dexterity}
+      (${item.dexterity < '+' + 10 ? '-' + (5 - Math.floor(item.dexterity / 2)) : item.dexterity > 11 ? Math.floor((item.dexterity - 10) / 2) : 0})
+      ${ item.dexterity_save ? ' - Save: ' + item.dexterity_save : ''}</p>
+    `);
+    $listData.append(`
+      <p>Constitution: ${item.constitution}
+      (+${item.constitution < 10 ? '-' + (5 - Math.floor(item.constitution / 2)) : item.constitution > 11 ? Math.floor((item.constitution - 10) / 2) : 0})
+      ${item.constitution_save ? ' - Save: ' + item.constitution_save : ''}</p>
+    `);
+    $listData.append(`
+      <p>Intelligence: ${item.intelligence}
+      (+${item.intelligence < 10 ? '-' + (5 - Math.floor(item.intelligence / 2)) : item.intelligence > 11 ? Math.floor((item.intelligence - 10) / 2) : 0})
+      ${item.intelligence_save ? ' - Save: ' + item.intelligence_save : ''}</p>
+    `);
+    $listData.append(`
+      <p>Wisdom: ${item.wisdom}
+      (+${item.wisdom < 10 ? '-' + (5 - Math.floor(item.wisdom / 2)) : item.wisdom > 11 ? Math.floor((item.wisdom - 10) / 2) : 0})
+      ${item.wisdom_save ? ' - Save: ' + item.wisdom_save : ''}</p>
+    `);
+    $listData.append(`
+      <p>Charisma: ${item.charisma}
+      (+${item.charisma < 10 ? '-' + (5 - Math.floor(item.charisma / 2)) : item.charisma > 11 ? Math.floor((item.charisma - 10) / 2) : 0}) 
+      ${item.charisma_save ? ' - Save: ' + item.charisma_save : ''}</p>
+      `);
+    if (item.perception) {
+      $listData.append(`
+        <p>Perception: ${item.perception}</p>
+        `);
+    }
+    if (item.languages !== "") {
+      $listData.append(`
+        <p>Languages: ${item.languages}</p>
+        `);
+    }
     $listData.append(`<p>
       <a href="https://www.google.com/search?safe=on&site=&tbm=isch&source=hp&biw=1440&bih=775&q=Dungeons+and+Dragons+${item.name.toLowerCase()}">
       Google Image Search
@@ -144,25 +207,43 @@ function createList(newData) {
     $listSup.append($listSupDiv);
     if (item.actions !== undefined) {
       for (var a = 0; a < item.actions.length; a++) {
-        $listSupDiv.append('<p>  Name: ' + item.actions[a].name + '</p>');
-        $listSupDiv.append('<p>  Attack Bonus: ' + item.actions[a].attack_bonus + '</p>');
-        $listSupDiv.append('<p>  Description: ' + item.actions[a].desc + '</p>');
+        $listSupDiv.append(`
+          <p>  Name: ${item.actions[a].name}</p>
+          `);
+        $listSupDiv.append(`
+          <p>  Attack Bonus: ${item.actions[a].attack_bonus}</p>
+          `);
+        $listSupDiv.append(`
+          <p>  Description: ${item.actions[a].desc}</p>
+          `);
         $listSupDiv.append('<p>--------------</p>');
       }
     }
     if (item.special_abilities !== undefined) {
       for (var a = 0; a < item.special_abilities.length; a++) {
-        $listSupDiv.append('<p>  Name: ' + item.special_abilities[a].name + '</p>');
-        $listSupDiv.append('<p>  Attack Bonus: ' + item.special_abilities[a].attack_bonus + '</p>');
-        $listSupDiv.append('<p>  Description: ' + item.special_abilities[a].desc + '</p>');
+        $listSupDiv.append(`
+          <p>  Name: ${item.special_abilities[a].name}</p>
+          `);
+        $listSupDiv.append(`
+          <p>  Attack Bonus: ${item.special_abilities[a].attack_bonus}</p>
+          `);
+        $listSupDiv.append(`
+          <p>  Description: ${item.special_abilities[a].desc}</p>
+          `);
         $listSupDiv.append('<p>--------------</p>');
       }
     }
     if (item.legendary_actions !== undefined) {
       for (var a = 0; a < item.legendary_actions.length; a++) {
-        $listSupDiv.append('<p>  Name: ' + item.legendary_actions[a].name + '</p>');
-        $listSupDiv.append('<p>  Attack Bonus: ' + item.legendary_actions[a].attack_bonus + '</p>');
-        $listSupDiv.append('<p>  Description: ' + item.legendary_actions[a].desc + '</p>');
+        $listSupDiv.append(`
+          <p>  Name: ${item.legendary_actions[a].name}</p>
+          `);
+        $listSupDiv.append(`
+          <p>  Attack Bonus: ${item.legendary_actions[a].attack_bonus}</p>
+          `);
+        $listSupDiv.append(`
+          <p>  Description: ${item.legendary_actions[a].desc}</p>
+          `);
         $listSupDiv.append('<p>--------------</p>');
       }
     }
